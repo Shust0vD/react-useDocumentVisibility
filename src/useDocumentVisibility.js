13fec38b9816fd269139ea95dc660d23c52f995c
Shust0vD <99994722+Shust0vD@users.noreply.github.com>
2022-08-03 18:31:23 +0300

    fix(useDocumentVisibility.js): удалил лишнюю очистку массива событий

--- src/useDocumentVisibility.js
+++ src/useDocumentVisibility.js
@@ -29,7 +29,6 @@ export default function useDocumentVisibility() {
     document.addEventListener('visibilitychange', checkVisible);
     return () => {
       document.removeEventListener('visibilitychange', checkVisible);
-      events.current = [];
     };
   }, [checkVisible]);
 
